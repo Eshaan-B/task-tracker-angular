53dc64033ac0d44accafc973d46de75f9efc5f0d
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2023-03-17 12:11:51 +0530

    added task list

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import Task from './Tasks';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Practive Angular',
+    day: 'March 17, 12.30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Doctor appointment',
+    day: 'March 3, 5.30pm',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Buy Groceries',
+    day: 'March 19, 5.00pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Major project review',
+    day: 'March 18, 11.00pm',
+    reminder: false,
+  },
+];
